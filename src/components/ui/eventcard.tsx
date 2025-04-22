a44431724af5686fe8bb72c988f87dba62542e1e
Danuja <danujadanuja25@gmail.com>
2025-04-22 21:48:30 +0530

    feat: add SVG graphic for event card and simplify Card component structure

--- src/components/ui/eventcard.tsx
+++ src/components/ui/eventcard.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-
 import { Electrolize } from "next/font/google";
+import Image from "next/image";
+import React from "react";
 
 const electrolize = Electrolize({
 	subsets: ["latin"],
@@ -10,105 +10,27 @@ const electrolize = Electrolize({
 type CardProps = {
 	title: string;
 	description: string;
-	image: string;
 };
 
-export default function Card({ title, description, image }: CardProps) {
+export default function Card({ title, description }: CardProps) {
 	return (
-		<div className="w-73 h-96 relative">
-			<div className="absolute top-18 left-20">
-				<div className="relative">
-					<div className="absolute transform rotate-45 bg-black w-33 h-39 -top-36 -left-36 z-1" />
-				</div>
-			</div>
-
-			<div className="rotate-225 absolute top-10 left-9 z-1">
-				<div className="absolute top-25 left-24">
-					<div className="relative">
-						<div className="absolute transform rotate-45 border-2 border-[#ed00da] w-24 h-24 -top-36 -left-36" />
-					</div>
-				</div>
-			</div>
-
-			<div className="rotate-225 absolute top-10 left-9 z-5">
-				<div className="absolute top-[115px] left-[42.99px]">
-					<div className="relative">
-						<div className="absolute transform rotate-90 border-t-2 border-[#ed00da] w-34 h-17 -top-36 -left-36" />
-					</div>
-				</div>
-			</div>
-
-			<div className="absolute top-[223px] left-[137.9px] z-5">
-				<div className="relative">
-					<div className="absolute transform rotate-225 bg-black w-2 h-3 -top-36 -left-36" />
-				</div>
-			</div>
-
-			<div className="absolute top-[126.9px] left-[230px] z-5">
-				<div className="relative">
-					<div className="absolute transform rotate-225 bg-black w-2 h-5 -top-36 -left-36" />
-				</div>
-			</div>
-
-			<div className="absolute top-[126.9px] left-[230px] z-5">
-				<div className="relative">
-					<div className="absolute transform rotate-225 bg-black w-2 h-2 -top-36 -left-36" />
-				</div>
-			</div>
-
-			<div className="w-full h-full relative overflow-hidden border-2 border-[#ed00da] rounded-sm p-3 pb-9">
-				<div className="absolute top-0 left-0">
-					<div className="relative">
-						<div className="absolute transform rotate-45 bg-black w-60 h-60 -top-36 -left-36 z-4" />
-					</div>
-				</div>
-
-				<div className="absolute -top-2 left-0">
-					<div className="relative">
-						<div className="absolute transform rotate-45 border-2 border-[#ed00da]  w-55 h-65 -top-36 -left-36 z-4" />
-					</div>
-				</div>
-
-				<div className="bg-white h-2/4 w-full relative z-3">{image}</div>
-
-				<div className={`bg-black h-1/4 w-full pt-3 ${electrolize.className}`}>
-					<h2 className="text-cyan-200 text-2xl font-medium">{title}</h2>
-					<p className="text-white text-sm text-justify">{description}</p>
-				</div>
-			</div>
-
-			<div className="absolute bottom-0 left-5">
-				<div className="relative">
-					<div className="absolute transform bg-[#ed00da] w-33 h-2 z-1" />
-				</div>
-			</div>
-			<div className="absolute -bottom-1 left-[9px]">
-				<div className="relative">
-					<div className="absolute transform bg-black rotate-60 w-4 h-4 z-1" />
-				</div>
-			</div>
-			<div className="absolute -bottom-1 right-[145px]">
-				<div className="relative">
-					<div className="absolute transform bg-black rotate-30 w-4 h-4 z-1" />
-				</div>
-			</div>
-
-			<div className="absolute top-45 right-[115.99px]">
-				<div className="relative">
-					<div className="absolute transform rotate-90 bg-[#ed00da] w-60 h-2 z-1" />
-				</div>
-			</div>
-
-			<div className="absolute top-74 right-[-3px]">
-				<div className="relative">
-					<div className="absolute transform bg-black rotate-45 w-4 h-4 z-1" />
-				</div>
-			</div>
-
-			<div className="absolute top-[54.9px] right-[-3px]">
-				<div className="relative">
-					<div className="absolute transform bg-black rotate-45 w-4 h-4 z-1" />
-				</div>
+		<div className="relative w-full max-w-[300px]">
+			<Image
+				src="/Frame 20-3.svg"
+				alt="event"
+				width={300}
+				height={500}
+				className="relative z-0"
+			/>
+			<div className="absolute top-0 left-0 w-full h-full z-10 flex flex-col justify-center px-6">
+				<h3
+					className={`${electrolize.className} text-xl text-[#a2ebff] text-start font-bold mb-2 pt-44`}
+				>
+					{title}
+				</h3>
+				<p className={`text-white text-sm ${electrolize.className}`}>
+					{description}
+				</p>
 			</div>
 		</div>
 	);
