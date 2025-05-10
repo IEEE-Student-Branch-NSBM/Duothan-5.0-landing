20b8a8cd1cb34685d568d74351111184523cc6c5
Tharinda Pathirana <tharindapathirana27@gmail.com>
2025-05-10 22:57:43 +0530

    feat: background-removed GIF"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
 			<AboutUs />
 			<PastEvents />
 			<div className="relative w-full bg-cover bg-top bg-no-repeat">
-				<div className="absolute inset-0 bg-[url('/background.gif')] bg-cover bg-top bg-no-repeat opacity-70 z-0" />
 				<Contactus />
 			</div>
 		</div>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,6 @@
 export default function Footer() {
 	return (
 		<footer className="relative w-full h-[104px] overflow-hidden">
-			<div className="absolute inset-0 bg-[url('/background.gif')] bg-no-repeat bg-cover bg-bottom opacity-70 z-0" />
-
 			<div className="relative z-10 text-white backdrop-blur-[30px] font-[Electrolize] text-center px-4 py-6 text-xs sm:text-sm w-full h-full flex items-center justify-center">
 				<p className="leading-snug">
 					Copyright © 2025, Developed by IEEE Computer Society of NSBM
