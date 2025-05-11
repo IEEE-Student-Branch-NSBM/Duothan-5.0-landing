df1c483ee857f86fccdbbe905d764f24bd345d62
animidox <dhanujadisanayke@gmail.com>
2025-05-12 03:23:33 +0530

    feat(sponsors): implement sponsor section with responsive cards and SVG logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import AboutUs from "@/components/about-us/aboutus";
 import Contactus from "@/components/contactus/contactus";
 import PastEvents from "@/components/pastevents/pastevents";
+import Sponsors from "@/components/sponsors/sponsors";
 
 export default function Home() {
 	return (
 		<div>
 			<div className="min-h-screen">Hero</div>
 			<AboutUs />
+			<Sponsors />
 			<PastEvents />
 			<Contactus />
 		</div>
