1f805ff072b7c27416b7d420f5e6930329a4bc08
RajanithBandara <RajanithBandara123@gmail.com>
2025-05-12 15:51:25 +0530

    feat: updated the page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AboutUs from "@/components/about-us/aboutus";
 import Contactus from "@/components/contactus/contactus";
 import Countdown from "@/components/countdown/Countdown";
 import PastEvents from "@/components/pastevents/pastevents";
+import RegistrationBanner from "@/components/register-page/RegistrationBanner";
 import Timeline from "@/components/timeline/timeline";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
 			<div className="min-h-screen">Hero</div>
 			<Countdown />
 			<AboutUs />
+			<RegistrationBanner />
 			<PastEvents />
 			<Timeline />
 			<div className="relative w-full bg-cover bg-top bg-no-repeat">
