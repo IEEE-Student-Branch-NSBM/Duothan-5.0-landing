2c077512485ddd36cc6f4282c49c17e521f8bb04
Janith Wathsala <janijaniwathsala@gmail.com>
2025-05-14 00:11:10 +0530

    feat: update timeline

--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -3,14 +3,7 @@ import { useState } from "react";
 import Card from "./timelinecard";
 
 export default function Timeline() {
-	type TimelineItem = {
-		id: number;
-		position: "left" | "right";
-		title: string;
-		description: string;
-	};
-
-	const [items] = useState<TimelineItem[]>([
+	const [items] = useState([
 		{
 			id: 1,
 			position: "left",
@@ -38,69 +31,108 @@ export default function Timeline() {
 	]);
 
 	return (
-		<div className="bg-non px-4 lg:px-0 flex flex-col items-center  relative w-full max-w-[1170px] mx-auto mb-10">
-			{/* Desktop view: Timeline with centered vertical line and cards on alternating sides */}
-			<div className="hidden md:flex flex-col justify-center relative w-full max-w-[1170px] mx-auto">
-				{/* Center timeline - desktop */}
-				<div className="absolute left-1/2 transform -translate-x-1/2  fix">
+		<div className="bg-non px-4 lg:px-0 flex flex-col items-center relative w-full max-w-[1170px] mx-auto mb-40">
+			{/* Desktop view */}
+			<div className="hidden lg:flex relative w-full max-w-[1170px] mx-auto">
+				<div className="absolute left-1/2 transform -translate-x-1/2">
 					<img
 						src="/timelinecompleet.svg"
 						alt="Timeline"
 						className="h-full object-contain"
 					/>
 				</div>
-
-				{/* Timeline items in a column - desktop view */}
-				<div className="flex flex-col w-full">
-					<div className="flex justify-end w-1/2 ml-auto">
-						<Card
-							title={items[0].title}
-							description={items[0].description}
-							position="right"
-						/>
+				<div className="flex flex-row w-full">
+					<div className="flex flex-col w-full">
+						{items
+							.filter((_, i) => i % 2 === 1)
+							.map((item, index) => (
+								<div
+									key={item.id}
+									className={`flex justify-start w-full ml-5 ${index === 0 ? "mt-47" : "mt-14"}`}
+								>
+									<Card
+										title={item.title}
+										description={item.description}
+										position="left"
+									/>
+								</div>
+							))}
 					</div>
-
-					<div className="flex justify-start w-1/2">
-						<Card
-							title={items[1].title}
-							description={items[1].description}
-							position="left"
-						/>
-					</div>
-
-					<div className="flex justify-end w-1/2 ml-auto">
-						<Card
-							title={items[2].title}
-							description={items[2].description}
-							position="right"
-						/>
-					</div>
-
-					<div className="flex justify-start w-1/2">
-						<Card
-							title={items[3].title}
-							description={items[3].description}
-							position="left"
-						/>
+					<div className="flex flex-col w-full">
+						{items
+							.filter((_, i) => i % 2 === 0)
+							.map((item, index) => (
+								<div
+									key={item.id}
+									className={`flex justify-start w-full ml-auto ${index === 0 ? "mt-13" : "mt-12"}`}
+								>
+									<Card
+										title={item.title}
+										description={item.description}
+										position="right"
+									/>
+								</div>
+							))}
 					</div>
 				</div>
 			</div>
 
-			{/* Mobile view: Vertical timeline with all cards on the right */}
-			<div className="md:hidden flex flex-col items-start justify-center relative w-full max-w-[500px] mx-auto">
-				{/* Left-aligned timeline - mobile */}
-				<div className="absolute left-4 top-0 h-full">
+			{/* Tablet view */}
+			<div className="hidden md:flex lg:hidden relative w-full max-w-[768px] mx-auto">
+				<div className="absolute left-1/2 transform -translate-x-1/2">
+					<img
+						src="/timelinecompleet.svg"
+						alt="Timeline"
+						className="h-full object-contain"
+					/>
+				</div>
+				<div className="flex flex-row w-full">
+					<div className="flex flex-col w-full">
+						{[items[1], items[3]].map((item, index) => (
+							<div
+								key={item.id}
+								className={`flex justify-start w-full ml-2 ${index === 0 ? "mt-58" : "mt-34"}`}
+							>
+								<Card
+									title={item.title}
+									description={item.description}
+									position="left"
+								/>
+							</div>
+						))}
+					</div>
+					<div className="flex flex-col w-full">
+						{[items[0], items[2]].map((item, index) => (
+							<div
+								key={item.id}
+								className={`flex justify-start w-full ml-auto ${index === 0 ? "mt-25" : "mt-32"}`}
+							>
+								<Card
+									title={item.title}
+									description={item.description}
+									position="right"
+								/>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+
+			{/* Mobile view */}
+			<div className="md:hidden flex items-start relative w-full max-w-[448px] mx-auto">
+				<div className="absolute left-4 top-0 h-146">
 					<img
 						src="/mobileTimeline.svg"
 						alt="Mobile Timeline"
 						className="h-full object-contain"
 					/>
 				</div>
-
-				{/* Timeline items in a single column for mobile */}
-				<div className="flex flex-col w-full pl-12 pb-3 space-y-6">
-					{items.map((item) => (
-						<div key={item.id} className="flex flex-col">
+				<div className="flex flex-col w-full">
+					{items.map((item, index) => (
+						<div
+							key={item.id}
+							className={`flex justify-start w-3/4 ml-5 ${index === 0 ? "mt-16" : index === 1 ? "mt-4.5" : "mt-4"}`}
+						>
 							<Card
 								title={item.title}
 								description={item.description}
