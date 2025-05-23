ba634fed2977835d2a261fb63b1e13c57fff9acf
Imesh Gimshan <iamcyklon@gmail.com>
2025-05-23 18:02:16 +0530

    feat: add header component for the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AboutUs from "@/components/about-us/aboutus";
 import Contactus from "@/components/contactus/contactus";
 import Countdown from "@/components/countdown/Countdown";
+import Header from "@/components/header/header";
 import PastEvents from "@/components/pastevents/pastevents";
 import RegistrationBanner from "@/components/register-page/RegistrationBanner";
 import Timeline from "@/components/timeline/timeline";
@@ -8,6 +9,7 @@ import Timeline from "@/components/timeline/timeline";
 export default function Home() {
 	return (
 		<div>
+			<Header />
 			<div className="min-h-screen">Hero</div>
 			<Countdown />
 			<AboutUs />
