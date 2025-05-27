7a104da122d956d7d7629ab8ab1062cd71bed025
Zerone Brianstorm <ometha12@gmail.com>
2025-05-27 08:56:24 +0530

    fix: test fixes for assets issue

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,16 @@ const nextConfig: NextConfig = {
 	output: "export",
 	basePath: process.env.NODE_ENV === "production" ? "/Duothan-5.0-landing" : "",
 	assetPrefix:
-		process.env.NODE_ENV === "production" ? "/Duothan-5.0-landing/" : "",
+		process.env.NODE_ENV === "production" ? "/Duothan-5.0-landing" : "",
 	images: {
 		unoptimized: true,
 		remotePatterns: [],
-		path:
-			process.env.NODE_ENV === "production"
-				? "https://ieee-student-branch-nsbm.github.io/Duothan-5.0-landing"
-				: "",
 	},
 	trailingSlash: true,
+	publicRuntimeConfig: {
+		basePath:
+			process.env.NODE_ENV === "production" ? "/Duothan-5.0-landing" : "",
+	},
 };
 
 export default nextConfig;
