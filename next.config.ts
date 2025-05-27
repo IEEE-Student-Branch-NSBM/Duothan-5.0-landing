518ad7ba652374e4b608d57316273cfae775b431
Zerone Brianstorm <ometha12@gmail.com>
2025-05-27 08:41:54 +0530

    fix: fixes assets issues

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export",
-	basePath: process.env.PAGES_BASE_PATH,
+	basePath: process.env.NODE_ENV === "production" ? "/Duothan-5.0-landing" : "",
+	assetPrefix:
+		process.env.NODE_ENV === "production" ? "/Duothan-5.0-landing/" : "",
 	images: {
 		unoptimized: true,
 	},
